cb74422eaec5992017345c1b70c5fba066d219d6
markb <markbuchholz8@gmail.com>
2023-07-24 20:01:20 -0600

    added final sql

--- backup.sql
+++ backup.sql
@@ -28,7 +28,7 @@ CREATE TABLE `friends` (
   `friend_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `friends_un` (`friend_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `friends` (
 
 LOCK TABLES `friends` WRITE;
 /*!40000 ALTER TABLE `friends` DISABLE KEYS */;
-INSERT INTO `friends` VALUES (3,1,6);
+INSERT INTO `friends` VALUES (36,1,6);
 /*!40000 ALTER TABLE `friends` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -62,7 +62,7 @@ CREATE TABLE `groups` (
   UNIQUE KEY `groups_token_un` (`group_token`) USING HASH,
   KEY `groups_FK` (`group_owner_id`),
   CONSTRAINT `groups_FK` FOREIGN KEY (`group_owner_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +71,7 @@ CREATE TABLE `groups` (
 
 LOCK TABLES `groups` WRITE;
 /*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-INSERT INTO `groups` VALUES (1,'token',1,'https://images.pexels.com/photos/2263936/pexels-photo-2263936.jpeg?auto=compress&cs=tinysrgb&w=1600','this is the first ever group','2023-07-04','First Group','https://images.pexels.com/photos/2055100/pexels-photo-2055100.jpeg?auto=compress&cs=tinysrgb&w=1600'),(16,'M6FYb2v2M1WDFSCB',1,'https://images.pexels.com/photos/16952091/pexels-photo-16952091/free-photo-of-wood-landscape-field-summer.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','new group','2023-07-12','a new group','https://images.pexels.com/photos/16877637/pexels-photo-16877637/free-photo-of-close-up-of-a-white-clover-flower.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load'),(17,'YIFU61BORjOCOBqE',1,'https://images.pexels.com/photos/17422313/pexels-photo-17422313/free-photo-of-sea-beach-vacation-love.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','this is totally new','2023-07-12','even newer','https://images.pexels.com/photos/16448468/pexels-photo-16448468/free-photo-of-blue-polaroid-photograph-of-flowers.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load'),(18,'MyKjYsd5neah8Eup',1,'https://images.pexels.com/photos/13324150/pexels-photo-13324150.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','this is brand new','2023-07-13','zebra','https://images.pexels.com/photos/13324150/pexels-photo-13324150.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
+INSERT INTO `groups` VALUES (1,'token',1,'https://images.pexels.com/photos/2263936/pexels-photo-2263936.jpeg?auto=compress&cs=tinysrgb&w=1600','this is the first ever group','2023-07-04','First Group','https://images.pexels.com/photos/2055100/pexels-photo-2055100.jpeg?auto=compress&cs=tinysrgb&w=1600'),(16,'M6FYb2v2M1WDFSCB',1,'https://images.pexels.com/photos/16952091/pexels-photo-16952091/free-photo-of-wood-landscape-field-summer.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','new group','2023-07-12','a new group','https://images.pexels.com/photos/16877637/pexels-photo-16877637/free-photo-of-close-up-of-a-white-clover-flower.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load'),(17,'YIFU61BORjOCOBqE',1,'https://images.pexels.com/photos/17422313/pexels-photo-17422313/free-photo-of-sea-beach-vacation-love.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','this is totally new','2023-07-12','even newer','https://images.pexels.com/photos/16448468/pexels-photo-16448468/free-photo-of-blue-polaroid-photograph-of-flowers.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load'),(18,'MyKjYsd5neah8Eup',1,'https://images.pexels.com/photos/13324150/pexels-photo-13324150.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','this is brand new','2023-07-13','zebra','https://images.pexels.com/photos/13324150/pexels-photo-13324150.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load'),(19,'join',1,'https://images.pexels.com/photos/17671746/pexels-photo-17671746/free-photo-of-snow-wood-landscape-mountains.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','fresh group','2023-07-24','F-G','https://images.pexels.com/photos/17671746/pexels-photo-17671746/free-photo-of-snow-wood-landscape-mountains.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load'),(20,'mtVVEHINL76SRecb',1,'dasf','asdf','2023-07-24','asdfa','sdf');
 /*!40000 ALTER TABLE `groups` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -87,7 +87,7 @@ CREATE TABLE `groups_members` (
   `member_id` int(10) unsigned NOT NULL,
   `group_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,7 +96,7 @@ CREATE TABLE `groups_members` (
 
 LOCK TABLES `groups_members` WRITE;
 /*!40000 ALTER TABLE `groups_members` DISABLE KEYS */;
-INSERT INTO `groups_members` VALUES (42,1,17),(43,1,16),(44,1,1),(45,6,1),(47,19,1),(48,20,1);
+INSERT INTO `groups_members` VALUES (42,1,17),(43,1,16),(44,1,1),(45,6,1),(47,19,1),(48,20,1),(49,16,1),(50,1,19),(51,1,20);
 /*!40000 ALTER TABLE `groups_members` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,7 +116,7 @@ CREATE TABLE `messages` (
   PRIMARY KEY (`id`),
   KEY `messages_FK` (`group_id`),
   CONSTRAINT `messages_FK` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,7 +125,7 @@ CREATE TABLE `messages` (
 
 LOCK TABLES `messages` WRITE;
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` VALUES (1,'this is content for a message to a certian group',5,1,'2023-07-06'),(2,'does this go any where',1,1,'2023-07-13'),(11,'does this go any where',1,1,'2023-07-13'),(14,'does this go any where',1,1,'2023-07-13'),(17,'does this go any where',1,1,'2023-07-13'),(20,'This is the message content.',1,1,'2023-07-13'),(21,'so this will update',1,17,'2023-07-13'),(22,'this is tiger first message',1,1,'2023-07-13'),(23,'this is a different server',1,16,'2023-07-13'),(24,'sfdf',1,17,'2023-07-15'),(25,'this is ralph',19,1,'2023-07-15'),(26,'asdasd',1,17,'2023-07-18'),(27,'asd',1,17,'2023-07-18'),(28,'asdasd',1,17,'2023-07-18'),(29,'asd',1,17,'2023-07-18'),(30,'asd',1,1,'2023-07-18');
+INSERT INTO `messages` VALUES (1,'this is content for a message to a certian group',5,1,'2023-07-06'),(2,'does this go any where',1,1,'2023-07-13'),(11,'does this go any where',1,1,'2023-07-13'),(14,'does this go any where',1,1,'2023-07-13'),(17,'does this go any where',1,1,'2023-07-13'),(20,'This is the message content.',1,1,'2023-07-13'),(21,'so this will update',1,17,'2023-07-13'),(22,'this is tiger first message',1,1,'2023-07-13'),(23,'this is a different server',1,16,'2023-07-13'),(24,'sfdf',1,17,'2023-07-15'),(25,'this is ralph',19,1,'2023-07-15'),(26,'asdasd',1,17,'2023-07-18'),(27,'asd',1,17,'2023-07-18'),(28,'asdasd',1,17,'2023-07-18'),(29,'asd',1,17,'2023-07-18'),(30,'asd',1,1,'2023-07-18'),(31,'new',1,1,'2023-07-18'),(32,'hg',1,1,'2023-07-18');
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -142,7 +142,7 @@ CREATE TABLE `user_login` (
   `user_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_token_un` (`token`) USING HASH
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -151,7 +151,7 @@ CREATE TABLE `user_login` (
 
 LOCK TABLES `user_login` WRITE;
 /*!40000 ALTER TABLE `user_login` DISABLE KEYS */;
-INSERT INTO `user_login` VALUES (27,'aYirVv5HK5ncXhpD',1),(31,'H8SUfwwXQplyqkbN',1),(32,'lcoGw5Q4Qlj2D1TC',1),(33,'ACS9o9yXTHu0oUhs',1),(39,'DHbOq4YzqrDJOOpZ',1);
+INSERT INTO `user_login` VALUES (27,'aYirVv5HK5ncXhpD',1),(31,'H8SUfwwXQplyqkbN',1),(32,'lcoGw5Q4Qlj2D1TC',1),(33,'ACS9o9yXTHu0oUhs',1),(39,'DHbOq4YzqrDJOOpZ',1),(40,'cN6fwezzxiuAGf6h',1),(42,'B492OhWCKfQKH6mL',21),(43,'mdARUe4LMF1GMbzw',1),(44,'B87Ppe99V2TH6iWV',16),(45,'mL0Vyx1AxYGfWPPC',1),(47,'N4DLDMtjEFaGUzLY',1);
 /*!40000 ALTER TABLE `user_login` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -174,7 +174,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unq` (`email`),
   UNIQUE KEY `users_username_un` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -183,7 +183,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'ghostic','ghostic@place.com','password','https://images.pexels.com/photos/5422771/pexels-photo-5422771.jpeg?auto=compress&cs=tinysrgb&w=300','https://images.pexels.com/photos/970517/pexels-photo-970517.jpeg?auto=compress&cs=tinysrgb&w=300','2023-07-04','hi i am ghostic'),(5,'skeleton','skeleton@place.com','password','https://images.pexels.com/photos/3756616/pexels-photo-3756616.jpeg?auto=compress&cs=tinysrgb&w=1600','https://images.pexels.com/photos/5435178/pexels-photo-5435178.jpeg?auto=compress&cs=tinysrgb&w=1600','2023-07-04','hi i am skeleton'),(6,'bravo','bravo@place.com','password','https://images.pexels.com/photos/17359582/pexels-photo-17359582/free-photo-of-light-sea-dawn-sunset.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','https://images.pexels.com/photos/17359582/pexels-photo-17359582/free-photo-of-light-sea-dawn-sunset.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','2023-07-06','hi i am bravo'),(16,'alpha','alpha@place.com','password','https://images.pexels.com/photos/17137766/pexels-photo-17137766/free-photo-of-a-sailboat-on-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','https://images.pexels.com/photos/17137766/pexels-photo-17137766/free-photo-of-a-sailboat-on-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','2023-07-06','this is the alpha account'),(19,'ralph','ralph@place.com','password','https://images.pexels.com/photos/2150/sky-space-dark-galaxy.jpg?auto=compress&cs=tinysrgb&w=1600','https://images.pexels.com/photos/844297/pexels-photo-844297.jpeg?auto=compress&cs=tinysrgb&w=1600','2023-07-15','this is big guy ralph'),(20,'bob','bob@place.com','password','https://images.pexels.com/photos/6012907/pexels-photo-6012907.jpeg?auto=compress&cs=tinysrgb&w=1600','https://images.pexels.com/photos/924824/pexels-photo-924824.jpeg?auto=compress&cs=tinysrgb&w=1600','2023-07-18','this is bob');
+INSERT INTO `users` VALUES (1,'guest','ghostic@place.com','password','https://images.pexels.com/photos/5422771/pexels-photo-5422771.jpeg?auto=compress&cs=tinysrgb&w=300','https://images.pexels.com/photos/970517/pexels-photo-970517.jpeg?auto=compress&cs=tinysrgb&w=300','2023-07-04','hi i am a guest'),(5,'skeleton','skeleton@place.com','password','https://images.pexels.com/photos/3756616/pexels-photo-3756616.jpeg?auto=compress&cs=tinysrgb&w=1600','https://images.pexels.com/photos/5435178/pexels-photo-5435178.jpeg?auto=compress&cs=tinysrgb&w=1600','2023-07-04','hi i am skeleton'),(6,'bravo','bravo@place.com','password','https://images.pexels.com/photos/17359582/pexels-photo-17359582/free-photo-of-light-sea-dawn-sunset.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','https://images.pexels.com/photos/17359582/pexels-photo-17359582/free-photo-of-light-sea-dawn-sunset.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','2023-07-06','hi i am bravo'),(16,'alpha','alpha@place.com','password','https://images.pexels.com/photos/17137766/pexels-photo-17137766/free-photo-of-a-sailboat-on-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','https://images.pexels.com/photos/17137766/pexels-photo-17137766/free-photo-of-a-sailboat-on-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load','2023-07-06','this is the alpha account'),(19,'ralph','ralph@place.com','password','https://images.pexels.com/photos/2150/sky-space-dark-galaxy.jpg?auto=compress&cs=tinysrgb&w=1600','https://images.pexels.com/photos/844297/pexels-photo-844297.jpeg?auto=compress&cs=tinysrgb&w=1600','2023-07-15','this is big guy ralph'),(20,'bob','bob@place.com','password','https://images.pexels.com/photos/6012907/pexels-photo-6012907.jpeg?auto=compress&cs=tinysrgb&w=1600','https://images.pexels.com/photos/924824/pexels-photo-924824.jpeg?auto=compress&cs=tinysrgb&w=1600','2023-07-18','this is bob'),(21,'user_one','userone@place.com','password','https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg?auto=compress&cs=tinysrgb&w=1600','https://images.pexels.com/photos/1631677/pexels-photo-1631677.jpeg?auto=compress&cs=tinysrgb&w=1600','2023-07-18','this is user one');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -233,7 +233,8 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `create_account_post`(username_input varchar(100), email_input varchar(100), password_input varchar(100), profile_img_input varchar(1000), banner_img_input varchar(1000), description_input varchar(250))
 begin
-	insert into users (username, email, password, profile_img, banner_img, created_at, description) values(username_input, email_input, password_input, profile_img_input, banner_img_input, (curdate()), description_input);
+	insert into users (username, email, password, profile_img, banner_img, created_at, description) 
+values(username_input, email_input, password_input, profile_img_input, banner_img_input, (curdate()), description_input);
 commit;
 END ;;
 DELIMITER ;
@@ -327,10 +328,18 @@ DELIMITER ;
 /*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `group_post`(group_token_input varchar(100), group_owner_id_input int, banner_img_input varchar(1000), description_input varchar(250), name_input varchar(100), profile_img_input varchar(1000))
-begin
-	insert into groups(group_token, group_owner_id, banner_img, description, created_at, name, profile_img) values (group_token_input, group_owner_id_input, banner_img_input, description_input, (curdate()), name_input, profile_img_input);
-SELECT CONVERT(group_token_input USING utf8) AS group_token;
-commit;
+BEGIN
+
+  INSERT INTO groups (group_token, group_owner_id, banner_img, description, created_at, name, profile_img)
+  VALUES (group_token_input, group_owner_id_input, banner_img_input, description_input, (curdate()), name_input, profile_img_input);
+
+  SET @new_group_id = LAST_INSERT_ID();
+
+  INSERT INTO groups_members (member_id, group_id) VALUES (group_owner_id_input, @new_group_id);
+
+  SELECT CONVERT(group_token_input USING utf8) AS group_token;
+  
+  COMMIT;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -495,31 +504,6 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `signle_group_get` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `signle_group_get`(IN group_id INT, IN member_id INT)
-BEGIN
-  SELECT group_token, banner_img, description, created_at, name, profile_img
-  FROM groups
-  WHERE id = group_id;
-
-  SELECT *
-  FROM groups_members
-  WHERE member_id = member_id;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `single_group_get` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -582,4 +566,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-18 15:59:28
+-- Dump completed on 2023-07-24 20:00:42
